796384cf5b43940ed25cbce25a3a36dee18eee4d
aturcino <adnan.turcinovic@gmail.com>
2014-05-21 23:33:59 +0200

    Additional formatting

--- zombiejs-playground.js
+++ zombiejs-playground.js
@@ -10,14 +10,14 @@ describe('Social icons', function() {
             browser.assert.element("#id1");
             browser.assert.element("#id2");
             console.log("Content:", browser.html());
-			browser.tabs.current = 1;
+	      browser.tabs.current = 1;
                 browser.clickLink('a.btn.social-linkedin', function(){
                     browser.
                         fill("testuser", user).
                         fill("testpass", pass).
                         pressButton('btn.login', function(){
                             browser.pressButton('input.ok', function(){
-									// handle cases for FB, Twitter... 
+				// handle cases for FB, Twitter... 
                             });
                         });
                 });
